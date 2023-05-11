cf620090d8132c4748257383adff2cc71e7c44ce
Malik Emiroğlu <malikemiroglu92@gmail.com>
2023-05-11 21:08:20 +0300

    devam edilecek

--- assets/js/uno.js
+++ assets/js/uno.js
@@ -1,15 +1,13 @@
 let value = ["0","1","2","3","4","5","6","7","8","9"];
-let colors = ['green', 'red', 'darkorange', 'dodgerblue'];
+let colors = ["green", "red", "darkorange", "dodgerblue"];
+let players = [];
+let cpu = [];
 let deck = [];
 let table = [];
+let playersTurn = ["player-1","player-2","player-3","player-4"];
+let turn = 1;
 
-let turn = 7000;
-let direction = 1;
-
-const players = [];
-const cpu = [];
-
-//kartlari desteye koyduk
+// kartlari desteye koyduk
 function createCards() {
     for(let color of colors){
         for(let i = 0; i < value.length; i++) {
@@ -21,237 +19,190 @@ function createCards() {
 };
 createCards();
 
-//kartlari karistirdik
+// desteyi karistirdik
 function shuffleCards() {
     deck.sort(() => Math.random() - 0.5);
 };
 shuffleCards();
 
-console.log(deck)
-
-// kartlari oyunculara dagitiyoruz
+// kartlari tum oyunculara dagitiyoruz
 function giveCardsToPlayer() {
     for (let j = 0 ; j < 4 ; j ++) {
-
         let playerCards = [];
         for (let i = 0 ; i < 7 ; i++) {
             playerCards.push(deck.pop());
         }
-
         players.push(playerCards);
     };
 };
 giveCardsToPlayer()
-console.log(players)
-console.log(deck)
 
+// ortaya ilk koyulan kart ayarlandi
+table.push(deck.pop());
 
-// ortaya ilk koyulan karti ayarlandi
-function startTable() {
-    for(let i = 0; i < deck.length; i++) {
-        if(!isNaN(parseInt(deck[i].value))) {
-            table.push(deck[i]);
-            deck.splice(i,1);
-            return;
-        };
-    };
-};
-startTable();
-console.log(table)
-console.log(deck)
-
-// ortada cekilecek kartlar 
-function renderDeck() {
-    let deckObject = document.querySelector('#deck');
-
-    // destede ki 0. indexteki karti tuttuk (burada sanki bi hata var bakicam)
-    let carta = deck[1];
-
-    console.log(carta)
-
-
-    deckObject.innerHTML = `
-        <div class="card cardBackground">
-                
+// kullanicilar icin kartlarin html kodlarini hazirladik
+function createCardHtml(color, value) {
+        return `
+        <div style="background-color:${color};" data-color="${color}" data-value="${value}" class="card">
+            <span class="topNumber">${value}</span>
+            <span class="underNumber">${value}</span>
+            <span class="centerNumberBg">${value}</span>
+            <span style="color:${color};" class="centerNumber">${value}</span>
         </div>
-
-        <div class="card frente" style="background-color: ${carta.color};">
-            <span class="topNumber">${carta.value}</span>
-            <span class="underNumber">${carta.value}</span>
-            <span class="centerNumber">${carta.value}</span>
-        </div>
-    `
+        `
 };
-renderDeck();
 
-
-const deckShow = document.querySelector('.deck');
-deckShow.addEventListener('click', function(e){
-    const element = e.target;
-    console.log(element)
-});
-
-const play = document.querySelector('#cardsContainer');
-
-play.addEventListener('click',function(e){
-    let lastCardOnTable = table[table.length - 1];
-
-    let playerCardColors = [];
-    
-    for(let i of players[0]){
-        playerCardColors += i.color + ",";
-    }
-    // playerCardsArray karsilastirmam gereken renkler
-    let playerColorArray = playerCardColors.split(",");
-    playerColorArray.pop();
-
-    let playerCardValues = [];
-    for(let i of players[0]){
-        playerCardValues += i.value + ","
-    }
-    let playerValueArray = playerCardValues.split(",");
-    playerValueArray.pop();
-    console.log(playerValueArray)
-
-    // index'i yakalatmam gerekiyor sonra o yakalattigim indexe gore karsilastirmam gerekiyor sanirim
-
-
-    for(let i = 0; i < playerValueArray.lengt; i++)
-    if(lastCardOnTable.value == playerValueArray){
-        console.log("bu kart atılcak")
-    }
-
-    
-    
-});
-
-
-
-// ortaya atilan kartlar
-function renderTable() {
-    let tableCardObject = document.querySelector('#table');
-    let numCards = tableCardObject.children.length;
-    
-    let carta = table[table.length - 1];
-    console.log(carta)
-
-    let angle = (Math.random() * -15);
-    console.log(angle)
-
-    tableCardObject.innerHTML += `
-        <div style="background-color: ${carta.color}; transform: rotateZ(${angle}deg);" class="card">
-            <span class="topNumber">${carta.value}</span>
-            <span class="underNumber">${carta.value}</span>
-            <span class="centerNumberBg">${carta.value}</span>
-            <span style="color:${carta.color};" class="centerNumber">${carta.value}</span>
-        </div>
-    `
-
-    numCards = tableCardObject.children.length;
-    if(numCards == 6) tableCardObject.children[0].remove();
-    
-};
-renderTable()
+let cardsContainer = document.querySelector('#cardsContainer');
+let cpuTwo = document.querySelector('#player-2');
+let cpuThree = document.querySelector('#player-3');
+let cpuFour = document.querySelector('#player-4');
+let deckObject = document.querySelector('#deck');
+let tableCardObject = document.querySelector('#table');
 
 // kullanicilarin eline kartlari dagitiyoruz
 function renderUserCards() {
-    let cardsContainer = document.querySelector('#cardsContainer');
-    let cpuTwo = document.querySelector('#player-2');
-    let cpuThree = document.querySelector('#player-3');
-    let cpuFour = document.querySelector('#player-4');
-    cardsContainer.innerHTML = "";
-    cpu.innerHTML = "";
-
-    for(let carta of players[0]) {
-        cardsContainer.innerHTML += `
-            <div style="background-color: ${carta.color};" class="card"">
-                <span class="topNumber">${carta.value}</span>
-                <span class="underNumber">${carta.value}</span>
-                <span class="centerNumberBg">${carta.value}</span>
-                <span style="color:${carta.color};" class="centerNumber">${carta.value}</span>
-            </div>
-        `;
+    for(let card of players[0]) {
+        cardsContainer.innerHTML += createCardHtml(card.color,card.value);
     };
 
-    for(let carta of players[1]) {
-        cpuTwo.innerHTML += `
-        <div style="background-color: ${carta.color};" class="card"">
-            <span class="topNumber">${carta.value}</span>
-            <span class="underNumber">${carta.value}</span>
-            <span class="centerNumberBg">${carta.value}</span>
-            <span style="color:${carta.color};" class="centerNumber">${carta.value}</span>
-        </div>
-        `;
+    for(let card of players[1]) {
+        cpuTwo.innerHTML += createCardHtml(card.color,card.value);
     };
 
-    for(let carta of players[2]) {
-        cpuThree.innerHTML += `
-        <div style="background-color: ${carta.color};" class="card"">
-            <span class="topNumber">${carta.value}</span>
-            <span class="underNumber">${carta.value}</span>
-            <span class="centerNumberBg">${carta.value}</span>
-            <span style="color:${carta.color};" class="centerNumber">${carta.value}</span>
-        </div>
-        `;
+    for(let card of players[2]) {
+        cpuThree.innerHTML += createCardHtml(card.color,card.value);
     };
 
-    for(let carta of players[3]) {
-        cpuFour.innerHTML += `
-        <div style="background-color: ${carta.color};" class="card"">
-            <span class="topNumber">${carta.value}</span>
-            <span class="underNumber">${carta.value}</span>
-            <span class="centerNumberBg">${carta.value}</span>
-            <span style="color:${carta.color};" class="centerNumber">${carta.value}</span>
-        </div>
-        `;
+    for(let card of players[3]) {
+        cpuFour.innerHTML += createCardHtml(card.color,card.value);
     };
 };
 renderUserCards();
 
-console.clear()
-
-/*
-// desteki karti oyuncuya cekmek icin
-function takeCard() {
-    let takenCard = [];
-
-    takenCard.push(deck[0]);
-    players[0].push(deck[0]);
-    deck.shift();
-
-    return takenCard[0];
-}
-takeCard();
-*/
-
-
-
-//#region 
-/*
-// kartlari karistirip desteye dagit
-for(let i = 0; i < colors.length; i++){
-    for(let j = 0; j < value.length; j++){
-        let cards = {value: value[j], color: colors[i]};
-        deck.push(cards);
-    };
+// table icin kartlarin html kodlarini hazirladik
+function createCardHtmlDeck(color,value) {
+    return `
+    <div class="card stackOfDeck" data-color="${color}" data-value="${value}" style="background-color:${color};">
+        <span class="topNumber">${value}</span>
+        <span class="underNumber">${value}</span>
+        <span class="centerNumberBg">${value}</span>
+        <span style="color:${color};" class="centerNumber">${value}</span>
+    </div>
+    `
 };
 
-// kartlari karistir
-let shuffleDeck = deck.sort(function() {
-    return Math.random() - 0.5
-});
-// function shuffleCards() {
-//     deck.sort(() => Math.random() - 0.5 );
-// }
-// kartlari oyunculara dagit
-let playerss = [[],[],[],[]];
-for(let i = 0; i < 7; i++){
-    for(let j = 0; j < 4; j++){
-        playerss[j].push(deck.pop());
+// ortadan cekilecek kartlar 
+function renderDeck() {
+    for(let card of deck){
+        deckObject.innerHTML += createCardHtmlDeck(card.color, card.value);
+    }
+};
+renderDeck();
+
+// ortaya atilan kartlar
+function renderTable() {
+    let carta = table[table.length - 1];
+    let angle = (Math.random() * -15);
+
+    tableCardObject.innerHTML += `
+        <div style="background-color:${carta.color}; transform: rotateZ(${angle}deg);" class="card">
+            <span class="topNumber">${carta.value}</span>
+            <span class="underNumber">${carta.value}</span>
+            <span class="centerNumberBg">${carta.value}</span>
+            <span style="color:${carta.color};" class="centerNumber">${carta.value}</span>
+        </div>
+    `
+};
+renderTable()
+
+let cardElements = document.querySelectorAll('.card');
+for(let cardElement of cardElements) {
+    cardElement.addEventListener('click', playCard);
+}
+
+function isCardPlayable(cardColor, cardValue) {
+    //let lastCardOnTable = document.querySelector('.table .card:last-child');
+    if(table[table.length-1].color === cardColor || table[table.length-1].value === cardValue) {
+        return true;
+    };
+    return false;
+};
+
+function changeTurn() {
+    if(turn === 1) {
+        turn = 2;
+    }else {
+        turn = 1;
+    }
+
+    isDeckUsed = false;
+}
+
+function canCurrentUserPlay() {
+    let currentPlayerCards;
+    if(turn === 1) {
+        currentPlayerCards = document.querySelectorAll('.cardsContainer .card');
+    }else {
+        currentPlayerCards = document.querySelectorAll('player-2 .card');
+    }
+    
+    for(let card of currentPlayerCards) {
+        if(isCardPlayable(card.dataset.color, card.dataset.value)) {
+            return true;
+        }
     }
 }
 
-table.push(deck.pop());
+let isDeckUsed = false; // deck'ten kart eklemeyi engellemek icin
 
-*/
-//#endregion
+function playCard() {
+
+    //table'a tiklatmiyorum.
+    if(this.parentNode.classList.contains('table')){
+        return;
+    }
+    
+    if(this.parentNode.classList.contains('deck')){
+        // isDeckUsed true olursa destemize deck'ten ikinci karti ekletmemek icin
+        if(isDeckUsed === true){
+            return;
+        }
+
+        let handToAppend = cpuTwo;
+        if (turn === 1){
+            handToAppend = cardsContainer;
+        }
+
+        this.classList.remove('stackOfDeck');
+        handToAppend.appendChild(this);
+        isDeckUsed = true;
+
+        if(!canCurrentUserPlay()){
+            changeTurn();
+        }
+        return;
+    }
+
+    if(!isCardPlayable(this.dataset.color, this.dataset.value)) {
+        return;
+    };
+
+    // burasi düzeltilecek
+    if(turn === 1 && this.parentNode.classList.contains('player-2') || this.parentNode.classList.contains('player-3') || this.parentNode.classList.contains('player-4')){
+        return;
+    }
+
+    if(turn === 2 && this.parentNode.classList.contains('cardsContainer')){
+        return;
+    }
+
+    tableCardObject.appendChild(this);
+    table[table.length-1].color = this.dataset.color;
+    table[table.length-1].value = this.dataset.value;
+
+    
+
+    changeTurn();
+
+};
+
