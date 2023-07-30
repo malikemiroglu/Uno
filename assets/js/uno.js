af2ea4e895d189ba8f9a30d7fb95581b9696d840
Malik Emiroğlu <malikemiroglu92@gmail.com>
2023-07-30 19:58:58 +0300

    oyun bitişi düzeltildi

--- assets/js/uno.js
+++ assets/js/uno.js
@@ -1,4 +1,4 @@
-let value = ["0","1","2","3","4","5","6","7","8","9"];
+let value = ["1","1","1","1","1",   "1","1","1","1"];
 let colors = ["green", "red", "darkorange", "dodgerblue"];
 let players = [];
 let cpu = [];
@@ -10,7 +10,6 @@ let turn = 1;
 function createCards() {
     for(let color of colors){
         for(let i = 0; i < value.length; i++) {
-
             let card = {color: color, value: value[i]};
             deck.push(card);
         };
@@ -20,9 +19,7 @@ createCards();
 
 
 // desteyi karistirdik
-function shuffleCards() {
-    deck.sort(() => Math.random() - 0.5);
-};
+const shuffleCards = () => deck.sort(() => Math.random() - 0.5);
 shuffleCards();
 
 // kartlari tum oyunculara dagitiyoruz
@@ -44,7 +41,7 @@ table.push(deck.pop());
 function createCardHtml(color, value, isBack = false) {
     if(isBack) {
         return `
-        <div class="card back" data-color="${color}" style="background-color:${color};"  data-color="${color}" data-value="${value}">
+        <div class="card back" style="background-color:${color};" data-color="${color}" data-value="${value}">
             <span class="topNumber">${value}</span>
             <span class="underNumber">${value}</span>
             <span class="centerNumberBg centerNumberBgOff">${value}</span>
@@ -61,8 +58,6 @@ function createCardHtml(color, value, isBack = false) {
         </div>
         `
     }
-
-
 };
 
 let cardsContainer = document.querySelector('#cardsContainer');
@@ -79,50 +74,50 @@ function renderUserCards() {
     };
 
     for(let card of players[1]) {
-        cpuTwo.innerHTML += createCardHtml(card.color, card.value, true);
+        cpuTwo.innerHTML += createCardHtml(card.color, card.value, false);
     };
 
     for(let card of players[2]) {
-        cpuThree.innerHTML += createCardHtml(card.color,card.value, true);
+        cpuThree.innerHTML += createCardHtml(card.color,card.value, false);
     };
 
     for(let card of players[3]) {
-        cpuFour.innerHTML += createCardHtml(card.color,card.value, true);
+        cpuFour.innerHTML += createCardHtml(card.color,card.value, false);
     };
+
 };
 renderUserCards();
 
 // deck icin kartlarin html kodlarini hazirladik
 function createCardHtmlDeck(color,value, isBack = false) {
-    return `
+    return (`
     <div class="card stackOfDeck ${isBack ? 'back': ''}" data-color="${color}" data-value="${value}" style="background-color:${color};">
         <span class="topNumber">${value}</span>
         <span class="underNumber">${value}</span>
         <span class="centerNumberBg centerNumberBgOff">${value}</span>
         <span style="color:${color};" class="centerNumber">${value}</span>
-    </div>
-    `
+    </div>`)
 };
 
 // deckten cekilecek kartlar 
 function renderDeck() {
     for(let card of deck){
         deckObject.innerHTML += createCardHtmlDeck(card.color, card.value, true);
-    };
+    };    
 };
 renderDeck();
 
 // ortaya atilan kartlar
 function renderTable() {
-    let carta = table[table.length - 1];
+    let card = table[table.length - 1];
     let angle = (Math.random() * - 15);
     
     tableCardObject.innerHTML += `
-        <div style="background-color:${carta.color}; transform: rotateZ(${angle}deg);" class="card">
-            <span class="topNumber">${carta.value}</span>
-            <span class="underNumber">${carta.value}</span>
-            <span class="centerNumberBg">${carta.value}</span>
-            <span style="color:${carta.color};" class="centerNumber">${carta.value}</span>
+        <div style="background-color:${card.color}; transform: rotateZ(${angle}deg);" class="card" data-color="${card.color}" data-value="${card.value}">
+            <span class="topNumber">${card.value}</span>
+            <span class="underNumber">${card.value}</span>
+            <span class="centerNumberBg">${card.value}</span>
+            <span style="color:${card.color};" class="centerNumber">${card.value}</span>
         </div>
     `
 };
@@ -144,7 +139,6 @@ function isCardPlayable(cardColor, cardValue) {
 function changeTurn() {
     switch(turn){
         case 1:
-            // debugger;
             turn = 2;
             break;
         case 2:
@@ -237,55 +231,7 @@ function playCard() {
     }
 
     if(this.parentNode.classList.contains('deck')){
-        // isDeckUsed true olursa destemize deck'ten ikinci karti ekletmemek icin
-        if(isDeckUsed === true){
-            return;
-        }
-
-        //this = card clasını temsil ediyor burada
-        // sira kimdeyse desckteki karti secilen divin alt cocugu olarak atiyor
-        if (turn === 1){
-            cardsContainer.appendChild(this);
-            this.classList.remove('back', 'stackOfDeck');
-            this.children[2].classList.remove('centerNumberBgOff'); 
-            }else if(turn === 2) {
-                cpuTwo.appendChild(this);
-                this.classList.remove('stackOfDeck');
-            }else if(turn === 3) {
-                cpuThree.appendChild(this);
-                this.classList.remove('stackOfDeck');
-            }else if(turn === 4) {
-                cpuFour.appendChild(this);
-                this.classList.remove('stackOfDeck');
-            }
-        
-        isDeckUsed = true; // deck'ten ikinci karti eklemeyi engellemek icin
-        
-
-        if(!canCurrentUserPlay()){
-            changeTurn();
-            playerTurn();
-        }
-
-        if (turn !== 1) {
-            let currentPlayerCards;
-            switch (turn) {
-                case 2:
-                    currentPlayerCards = document.querySelectorAll('.player-2 .card');
-                    break;
-                case 3:
-                    currentPlayerCards = document.querySelectorAll('.player-3 .card');
-                    break;
-                case 4:
-                    currentPlayerCards = document.querySelectorAll('.player-4 .card');
-                    break;
-            }
-    
-            // CPU oyuncusu için hamleyi oyna
-            playCardByCPU(currentPlayerCards);
-        }
-
-
+        drawCards.call(this);
         return;
     }
 
@@ -297,16 +243,26 @@ function playCard() {
     if(turn === 3 && !this.parentNode.classList.contains('player-3')) return;
     if(turn === 4 && !this.parentNode.classList.contains('player-4')) return;
 
-    let playerCardContainer = this.parentNode;
-
     tableCardObject.appendChild(this);
-    table[table.length-1].color = this.dataset.color;
-    table[table.length-1].value = this.dataset.value;
+    table.push({ color: this.dataset.color, value: this.dataset.value });
 
-    if(playerCardContainer.children.length === 0) {
-        alert('Kazanan oyuncu: ' + turn);
-        cardElements.forEach(card => card.removeEventListener('click',playCard));
+    switch (turn) {
+        case 1:
+            removeFromPlayerCards(this, players[0]);
+            break;
+        case 2:
+            removeFromPlayerCards(this, players[1]);
+            break;
+        case 3:
+            removeFromPlayerCards(this, players[2]);
+            break;
+        case 4:
+            removeFromPlayerCards(this, players[3]);
+            break;
     }
+
+    checkGameEnd();
+
     changeTurn();
     playerTurn();
 
@@ -323,15 +279,90 @@ function playCard() {
                 currentPlayerCards = document.querySelectorAll('.player-4 .card');
                 break;
         }
+
         // CPU oyuncusu için hamleyi oyna
         playCardByCPU(currentPlayerCards);
     }
 };
 
+function drawCards() {
+    // isDeckUsed true olursa destemize deck'ten ikinci karti ekletmemek icin
+    if(isDeckUsed){
+        return;
+    }
+
+    //this = card clasını temsil ediyor burada
+    // sira kimdeyse desckteki karti secilen divin alt cocugu olarak atiyor
+    if (turn === 1){
+        cardsContainer.appendChild(this);
+        this.classList.remove('back', 'stackOfDeck');
+        this.children[2].classList.remove('centerNumberBgOff'); 
+        players[0].push({ color: this.dataset.color, value: this.dataset.value });
+    }else if(turn === 2) {
+        cpuTwo.appendChild(this);
+        this.classList.remove('stackOfDeck');
+        players[1].push({ color: this.dataset.color, value: this.dataset.value });
+    }else if(turn === 3) {
+        cpuThree.appendChild(this);
+        this.classList.remove('stackOfDeck');
+        players[2].push({ color: this.dataset.color, value: this.dataset.value });
+    }else if(turn === 4) {
+        cpuFour.appendChild(this);
+        this.classList.remove('stackOfDeck');
+        players[3].push({ color: this.dataset.color, value: this.dataset.value });
+    }
+    
+    isDeckUsed = true; // deck'ten ikinci karti eklemeyi engellemek icin
+    
+
+    if(!canCurrentUserPlay()){
+        changeTurn();
+        playerTurn();
+    }
+
+    if (turn !== 1) {
+        let currentPlayerCards;
+        switch (turn) {
+            case 2:
+                currentPlayerCards = document.querySelectorAll('.player-2 .card');
+                break;
+            case 3:
+                currentPlayerCards = document.querySelectorAll('.player-3 .card');
+                break;
+            case 4:
+                currentPlayerCards = document.querySelectorAll('.player-4 .card');
+                break;
+        }
+
+        // CPU oyuncusu için hamleyi oyna
+        playCardByCPU(currentPlayerCards);
+    }
+}
+
 function playCardByCPU(currentPlayerCards) {
     const cardsArray = Array.from(currentPlayerCards);
     const matchingCards = cardsArray.filter(card => isCardPlayable(card.dataset.color, card.dataset.value));
     const selectedCard = matchingCards.length ? matchingCards[Math.floor(Math.random() * matchingCards.length)] : document.querySelector('.deck .card:last-child');
-    setTimeout(() => selectedCard.click(), 1000);
+    setTimeout(() => {
+        selectedCard.click();
+    }, 1000);
 }
 
+function removeFromPlayerCards(card, playerCards) {
+    for (let i = 0; i < playerCards.length; i++) {
+        if (playerCards[i].color === card.dataset.color && playerCards[i].value === card.dataset.value) {
+            playerCards.splice(i, 1);
+            break;
+        }
+    }
+}
+
+function checkGameEnd() {
+    for (let i = 0; i < players.length; i++) {
+        if (players[i].length === 0) {
+            alert('Kazanan oyuncu: ' + (i + 1));
+            cardElements.forEach(card => card.removeEventListener('click', playCard));
+            return;
+        }
+    }
+}
